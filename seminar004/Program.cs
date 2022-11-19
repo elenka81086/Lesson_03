f911aff8947b09db5cd02ff4f5b44493559d445b
Elena <elenka81086@mail.ru>
2022-11-19 13:12:09 +0300

    Задача 8

---
+++ seminar004/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = new int();
+Console.WriteLine("Введите число");
+N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+Console.WriteLine("Все чётные числа");  
+while (i < N - 1)
+{
+    i += 2;
+    Console.WriteLine(i);
+}
+ 
+
+
