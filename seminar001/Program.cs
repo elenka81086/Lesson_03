c24708e19ba764680e84d7a848b988aa81285dc6
Elena <elenka81086@mail.ru>
2022-11-19 11:23:21 +0300

    Задача 2

---
+++ seminar001/Program.cs
@@ -0,0 +1,15 @@
+﻿int Number1 = new int();
+Console.WriteLine("Введите число");
+Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = new int();
+Console.WriteLine("Введите число");
+Number2 = Convert.ToInt32(Console.ReadLine());
+if(Number1 < Number2)
+{
+    Console.WriteLine(Number2);
+}
+else
+{
+    Console.Write("Большее число");
+    Console.WriteLine(Number1);
+}
